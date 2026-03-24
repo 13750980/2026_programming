de634c13c009bae3956356dd61070a69917149db
13750980 <13750980@me.mcu.edu.tw>
2026-03-24 11:14:56 +0800

    week05

---
+++ 13750980_week05/13750980_week05/13750980_week05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace week05
+{
+    class MainClass 
+    {
+
+        public static void Main() 
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Enter a year:");
+
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+
+                bool l = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+                if (l)
+                {
+                    Console.WriteLine($"{year} 是閏年");
+                }
+                else
+                {
+                    Console.WriteLine($"{year} 是平年");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Error year!!!");
+            }
+        }
+    }
+}
