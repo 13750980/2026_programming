db707d2cc4abe67c31f323254af47d640bd98675
13750980 <13750980@me.mcu.edu.tw>
2026-04-06 13:24:01 +0800

    week06

---
+++ 13750980_week06/week06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        Console.Write("Enter the first num:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the second num:");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"GCD：{calc.GCD(a, b)}");
+        Console.WriteLine($"LCM：{calc.LCM(a, b)}");
+    }
+}
+class Calculator
+{
+    public int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public int LCM(int a, int b)
+    {
+        return a / GCD(a, b) * b;
+    }
+}
