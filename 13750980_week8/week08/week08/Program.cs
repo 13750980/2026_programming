bcb091e7837494f0654201b898061ecbcfe65d61
13750980 <13750980@me.mcu.edu.tw>
2026-04-14 11:12:10 +0800

    week08

---
+++ 13750980_week8/week08/week08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int target = new Random().Next(1, 11);
+        int guess = 0;
+
+        Console.WriteLine("Guess 1-10:");
+
+        while (guess != target)
+        {
+            if (int.TryParse(Console.ReadLine(), out guess))
+            {
+                if (guess == target) Console.WriteLine("You did it!");
+                else Console.WriteLine("Wrong! Try again~");
+            }
+        }
+    }
+}
